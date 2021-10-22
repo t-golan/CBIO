2baaf167746ec7899280e4ed6de5f24fef76c9e7
rotem_gil <rotem,gil@mail.huji.ac.il>
2021-10-22 11:47:06 +0300

    created GlobalAlignment class, refactored and created print function

--- seq_align.py
+++ seq_align.py
@@ -3,8 +3,134 @@ import numpy as np
 from itertools import groupby
 import pandas as pd
 
-
 SCORE = 0
+COORD = 1
+GAP = '-'
+X = 0
+Y = 1
+
+class GlobalAlignment:
+    def __init__(self, score_df, seq_a, seq_b):
+        self.score_df = score_df
+        self.seq_a = seq_a
+        self.seq_b = seq_b
+
+        self.n = len(seq_a)
+        self.m = len(seq_b)
+        self.mat = np.ndarray(shape=(self.n, self.m), dtype=tuple)
+
+    def sigma(self, i, j):
+        # if i is not gap, gets corresponding letter from seq
+        if i == GAP:
+            a = i
+        else:
+            a = self.seq_a[i]
+        # if j is not gap, gets corresponding letter from seq
+        if j == GAP:
+            b = j
+        else:
+            b = self.seq_b[j]
+        return self.score_df[a][b]
+
+    def align(self):
+        for col in range(self.m):
+            for row in range(self.n):
+                # base case, assign score = 0 and prev coordinates 0,0
+                if row == 0 and col == 0:
+                    self.mat[row][col] = (0, (0, 0))
+                # first column has only the option of gaps
+                elif col == 0:
+                    opt_score = self.mat[row-1][col][SCORE] + self.sigma(row, GAP)
+                    self.mat[row][col] = (opt_score, (row-1, col))
+                # first row has only the option of gaps
+                elif row == 0:
+                    opt_score = self.mat[row][col - 1][SCORE] + self.sigma(GAP, col)
+                    self.mat[row][col] = (opt_score, (row, col - 1))
+                # all other blocks have three options
+                else:
+                    # for each of the three options create a tuple of score, coord
+                    options = [
+                    (self.mat[row-1][col][SCORE] + self.sigma(row, GAP), (row-1, col)),
+                    (self.mat[row][col-1][SCORE] + self.sigma(GAP, col), (row, col-1)),
+                    (self.mat[row-1][col-1][SCORE] + self.sigma(row, col), (row-1, col-1))]
+
+                    # get optimal tuple based on max score
+                    optimal = max(options, key=lambda option: option[SCORE])
+                    self.mat[row][col] = optimal
+
+    def get_strings(self):
+        coord = (self.n-1, self.m-1)
+        alignment_a = ''
+        alignment_b = ''
+        while coord != (0, 0):
+            prev_coord = self.mat[coord[X]][coord[Y]][COORD]
+            # diagonal case
+            if (prev_coord[X] == coord[X] - 1) and \
+                    (prev_coord[Y] == coord[Y] - 1):
+                alignment_a += self.seq_a[coord[X]]
+                alignment_b += self.seq_b[coord[Y]]
+            # horizontal case - progressed in seq_b but not in seq_a
+            elif (prev_coord[X] == coord[X] - 1) and \
+                    (prev_coord[Y] != coord[Y] - 1):
+                alignment_a += GAP
+                alignment_b += self.seq_b[coord[Y]]
+            # vertical case - progressed in seq_a but not in seq_b
+            elif (prev_coord[X] != coord[X] - 1) and \
+                    (prev_coord[Y] == coord[Y] - 1):
+                alignment_a += self.seq_a[coord[X]]
+                alignment_b += GAP
+            coord = prev_coord
+        # alignment is recovered in reverse order, [::-1] reverses strings back
+        return alignment_a[::-1], alignment_b[::-1]
+
+    def get_score(self):
+        return self.mat[self.n-1][self.m-1][SCORE]
+
+
+class LocalAlignment:
+    def __init__(self, score_df, seq_a, seq_b):
+        self.score_df = score_df
+        self.seq_a = seq_a
+        self.seq_b = seq_b
+
+        self.n = len(seq_a)
+        self.m = len(seq_b)
+        self.mat = np.ndarray(shape=(self.n, self.m), dtype=tuple)
+
+    def sigma(self, i, j):
+        pass
+
+    def align(self):
+        pass
+
+    def get_strings(self):
+        pass
+
+    def get_score(self):
+        pass
+
+class OverlapAlignment:
+    def __init__(self, score_df, seq_a, seq_b):
+        self.score_df = score_df
+        self.seq_a = seq_a
+        self.seq_b = seq_b
+
+        self.n = len(seq_a)
+        self.m = len(seq_b)
+        self.mat = np.ndarray(shape=(self.n, self.m), dtype=tuple)
+
+    def sigma(self, i, j):
+        pass
+
+    def align(self):
+        pass
+
+    def get_strings(self):
+        pass
+
+    def get_score(self):
+        pass
+
 
 def fastaread(fasta_name):
     """
@@ -19,6 +145,14 @@ def fastaread(fasta_name):
         seq = "".join(s.strip() for s in next(faiter))
         return header, seq
 
+
+def print_alignment(alignment_a, alignment_b, type, score):
+    # prints alignment strings in chunks of 50 chars
+    for i in range(0, max(len(alignment_a), len(alignment_b)), 50):
+        print(alignment_a[i:i + 50])
+        print(alignment_b[i:i + 50])
+        print()
+    print(f'{type}:{score}')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -36,87 +170,24 @@ def main():
     score_df = pd.read_csv(command_args.score, sep='\t', index_col=0)
     # fasta_head, fasta_seq = fastaread(command_args)
     if command_args.align_type == 'global':
-        alignment, score = global_seq(seq_a, seq_b, score_df)
+        g = GlobalAlignment(score_df, seq_a, seq_b)
+        g.align()
+        score = g.get_score()
+        str_a, str_b = g.get_strings()
+        print_alignment(str_a, str_b, command_args.align_type, score)
     elif command_args.align_type == 'local':
-        alignment, score = local_seq(seq_a, seq_b, score_df)
+        l = LocalAlignment(score_df, seq_a, seq_b)
+        #l.align()
+        #score = l.get_score()
+        #str_a, str_b = l.get_strings()
+        #print_alignment(str_a, str_b, command_args.align_type, score)
     elif command_args.align_type == 'overlap':
-        alignment, score = overlap_seq(seq_a, seq_b, score_df)
-    print((alignment, score))
+        o = OverlapAlignment(score_df, seq_a, seq_b)
+        #o.align()
+        score = o.get_score()
+        #str_a, str_b = o.get_strings()
+        #print_alignment(str_a, str_b, command_args.align_type, score)
 
-
-def sigma(score_df, seq_a, seq_b, i, j):
-    if (i == '-'):
-        a = '-'
-    else:
-        a = seq_a[i]
-    if (j == '-'):
-        b = '-'
-    else:
-        b = seq_b[j]
-    return score_df[a][b]
-
-
-def global_seq(seq_a, seq_b, score_df):
-    n = len(seq_a)
-    m = len(seq_b)
-    mat = np.ndarray(shape=(n, m), dtype=tuple)
-    mat[0][0] = (0, (0, 0))
-    for col in range(m):
-        for row in range(n):
-   #         print((row,col))
-            if row == 0 and col == 0:
-                mat[row][col] = (0, (0, 0))
-            elif row != 0 and col != 0:
-                o1 = mat[row-1][col][SCORE] + sigma(score_df, seq_a, seq_b, row, '-')
-                o2 = mat[row][col-1][SCORE] + sigma(score_df, seq_a, seq_b, '-', col)
-                o3 = mat[row-1][col-1][SCORE] + sigma(score_df, seq_a, seq_b, row, col)
-                print(max(o1, o2, o3))
-                idx = np.argmax(np.array([o1, o2, o3]))
-                if idx == 0:
-                    mat[row][col] = (o1, (row-1, col))
-                if idx == 1:
-                    mat[row][col] = (o2, (row, col-1))
-                if idx == 2:
-                    mat[row][col] = (o3, (row-1, col-1))
-            elif col == 0:
-                o1 = mat[row-1][col][SCORE] + sigma(score_df, seq_a, seq_b, row, '-')
-                mat[row][col] = (o1, (row-1, col))
-            elif row == 0:
-                o1 = mat[row][col-1][SCORE] + sigma(score_df, seq_a, seq_b, '-', col)
-                mat[row][col] = (o1, (row, col-1))
-    return get_alignment_result(seq_a, seq_b, mat, n, m)
-
-def get_alignment_result(seq_a, seq_b, mat, n, m):
-    score = mat[n-1][m-1][0]
-    coord = (n-1, m-1)
-    alignment_a = ''
-    alignment_b = ''
-    while coord != (0, 0):
-        prev_coord = mat[coord[0]][coord[1]][1]
-        # diagonal case
-        if (prev_coord[0] == coord[0] - 1) and (prev_coord[1] == coord[1] - 1):
-            alignment_a += seq_a[coord[0]]
-            alignment_b += seq_b[coord[1]]
-        # horizontal case - progressed in seq_b but not in seq_a
-        elif (prev_coord[0] == coord[0] - 1) and (
-                prev_coord[0] != coord[0] - 1):
-            alignment_a += '_'
-            alignment_b += seq_b[coord[1]]
-        # vertical case - progressed in seq_a but not in seq_b
-        elif (prev_coord[0] != coord[0] - 1) and (
-                prev_coord[0] == coord[0] - 1):
-            alignment_a += seq_a[coord[0]]
-            alignment_b += '_'
-        coord = prev_coord
-    return (alignment_a[::-1], alignment_b[::-1]), score
-
-
-def local_seq(seq_a, seq_b, score_df):
-    pass
-
-
-def overlap_seq(seq_a, seq_b, score_df):
-    pass
 
 
 if __name__ == '__main__':
