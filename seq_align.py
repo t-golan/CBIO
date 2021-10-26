322296634127e84515d47b9c3f3b948f802d3a53
rotem_gil <rotem,gil@mail.huji.ac.il>
2021-10-26 11:16:44 +0300

    with overlap

--- seq_align.py
+++ seq_align.py
@@ -139,6 +139,7 @@ class LocalAlignment:
         mat_df = pd.DataFrame(self.mat)
         score_mat = mat_df.applymap(lambda x: x[SCORE]).to_numpy()
         coord = np.unravel_index(score_mat.argmax(), score_mat.shape)
+
         prev_coord = self.mat[coord[X]][coord[Y]][COORD]
         alignment_a = ''
         alignment_b = ''
@@ -170,6 +171,7 @@ class LocalAlignment:
         return score_df.to_numpy().max()
 
 
+
 class OverlapAlignment:
     def __init__(self, score_df, seq_a, seq_b):
         self.score_df = score_df
@@ -178,19 +180,91 @@ class OverlapAlignment:
 
         self.n = len(seq_a)
         self.m = len(seq_b)
-        self.mat = np.ndarray(shape=(self.n, self.m), dtype=tuple)
+        self.mat = np.ndarray(shape=(self.n+1, self.m+1), dtype=tuple)
+
 
     def sigma(self, i, j):
-        pass
+        # if i is not gap, gets corresponding letter from seq
+        if i == GAP:
+            a = i
+        else:
+            a = self.seq_a[i-1]
+        # if j is not gap, gets corresponding letter from seq
+        if j == GAP:
+            b = j
+        else:
+            b = self.seq_b[j-1]
+        return self.score_df[a][b]
+
 
     def align(self):
-        pass
+        for col in range(self.m+1):
+            for row in range(self.n+1):
+                # base case, assign score = 0 and prev coordinates 0,0
+                if row == 0 and col == 0:
+                    self.mat[row][col] = (0, (0, 0))
+                # first column has only the option of gaps
+                elif col == 0:
+                    opt_score = self.mat[row-1][col][SCORE] + max(0,self.sigma(row, GAP))
+                    self.mat[row][col] = (opt_score, (row-1, col))
+                # first row has only the option of gaps
+                elif row == 0:
+                    opt_score = self.mat[row][col - 1][SCORE] + self.sigma(GAP, col)
+                    self.mat[row][col] = (opt_score, (row, col - 1))
+                # all other blocks have three options
+                elif row == self.n:
+                    options = [
+                        (self.mat[row-1][col][SCORE] + max(0, self.sigma(row, GAP)), (row-1, col)),
+                        (self.mat[row][col-1][SCORE] + max(0, self.sigma(GAP, col)), (row, col-1)),
+                        (self.mat[row-1][col-1][SCORE] + max(0,self.sigma(row, col)), (row-1, col-1))]
+                    # get optimal tuple based on max score
+                    optimal = max(options, key=lambda option: option[SCORE])
+                    self.mat[row][col] = optimal
+                else:
+                    # for each of the three options create a tuple of score, coord
+                    options = [
+                        (self.mat[row-1][col][SCORE] + self.sigma(row, GAP), (row-1, col)),
+                        (self.mat[row][col-1][SCORE] + self.sigma(GAP, col), (row, col-1)),
+                        (self.mat[row-1][col-1][SCORE] + self.sigma(row, col), (row-1, col-1))]
 
-    def get_strings(self):
-        pass
+                    # get optimal tuple based on max score
+                    optimal = max(options, key=lambda option: option[SCORE])
+                    self.mat[row][col] = optimal
 
-    def get_score(self):
-        pass
+
+    def get_strings(self, coord):
+        coord = coord
+        alignment_a = ''
+        alignment_b = ''
+        while coord != (0, 0):
+            prev_coord = self.mat[coord[X]][coord[Y]][COORD]
+            # diagonal case
+            if (prev_coord[X] == coord[X] - 1) and \
+                    (prev_coord[Y] == coord[Y] - 1):
+                alignment_a += self.seq_a[coord[X]-1]
+                alignment_b += self.seq_b[coord[Y]-1]
+            # horizontal case - progressed in seq_b but not in seq_a
+            elif (prev_coord[X] == coord[X] - 1) and \
+                    (prev_coord[Y] != coord[Y] - 1):
+                alignment_a += self.seq_a[coord[X]-1]
+                alignment_b += GAP
+            # vertical case - progressed in seq_a but not in seq_b
+            elif (prev_coord[X] != coord[X] - 1) and \
+                    (prev_coord[Y] == coord[Y] - 1):
+                alignment_a += GAP
+                alignment_b += self.seq_b[coord[Y]-1]
+            coord = prev_coord
+        # alignment is recovered in reverse order, [::-1] reverses strings back
+        return alignment_a[::-1], alignment_b[::-1]
+
+
+    def get_best(self):
+        mat_df = pd.DataFrame(self.mat[self.n][:])
+        score_df = mat_df.applymap(lambda x: x[SCORE])
+        scores = score_df.to_numpy()
+        coords = (self.n, scores.argmax())
+        return coords
+
 
 
 def fastaread(fasta_name):
@@ -244,10 +318,11 @@ def main():
         print_alignment(str_a, str_b, command_args.align_type, score)
     elif command_args.align_type == 'overlap':
         o = OverlapAlignment(score_df, seq_a, seq_b)
-        #o.align()
-        #score = o.get_score()
-        #str_a, str_b = o.get_strings()
-        #print_alignment(str_a, str_b, command_args.align_type, score)
+        o.align()
+        best = o.get_best()
+        score = o.mat[best][SCORE]
+        str_a, str_b = o.get_strings(best)
+        print_alignment(str_a, str_b, command_args.align_type, score)
 
 
 
